46b3b24d2016ef0e74b5bf5958c76fdfd44d919b
Seth Boyce <sethboyce02@gmail.com>
2023-01-28 22:26:06 -0600

    Add project files.

---
+++ Giraffe/Abacus.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Calculator.Program;
+
+namespace Giraffe
+{
+    public class Abacus
+    {
+        public int? Num1 { get; set; }
+        public int? Num2 { get; set; }
+        public Operation? Process { get; set; }
+
+        public Abacus(int num1, int num2, Operation? process) { 
+            Num1 = num1;
+            Num2 = num2;
+            Process = process;
+        }
+
+        public int? Calculate()
+        {
+            if (Num1 != null || Num2 != null)
+            {
+                if (Process != null)
+                {
+                    switch (Process)
+                    {
+                        case Operation.Addition:
+                            return Addition();
+
+                        case Operation.Subtraction:
+                            return Subtraction();
+
+                        case Operation.Multiplication:
+                            return Multiplication();
+
+                        case Operation.Division:
+                            return Division();
+                    }
+                    
+                }
+            }
+            else
+            {
+                Console.WriteLine("You stupid, use numba");
+            }
+            return null;
+        }
+
+        public int? Addition()
+        {
+            if (Num1 == null && Num2 == null) return null;
+
+            return Num1 + Num2;
+        }
+
+        public int? Subtraction()
+        {
+            if (Num1 == null && Num2 == null) return null;
+
+            return Num1 - Num2;
+        }
+
+        public int? Multiplication()
+        {
+            if (Num1 == null && Num2 == null) return null;
+
+            return Num1 * Num2;
+        }
+
+        public int? Division()
+        {
+            if (Num1 == null && Num2 == null) return null;
+
+            return Num1 / Num2;
+        }
+    }
+}
---
+++ Giraffe/Program.cs
@@ -0,0 +1,78 @@
+﻿using Giraffe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static Calculator.Program;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            do
+            {
+                Console.Write("Enter First Number:");
+                var num1successful = int.TryParse(Console.ReadLine(), out var num1);
+
+                Console.Write("Enter Second Number:");
+                var num2successful = int.TryParse(Console.ReadLine(), out var num2);
+
+                Console.Write("Enter Operation (/,+,-,*):");
+                string symbol = Console.ReadLine();
+
+                if (num1successful == false || num2successful == false)
+                {
+                    Console.WriteLine("Wrong number, try again you dingus.");
+                }
+                else
+                {
+                    Operation? operation = null;
+                    operation = FindOperation(symbol);
+
+                    var abacus = new Abacus(num1, num2, operation);
+                    var result = abacus.Calculate();
+                    if (result != null)
+                    {
+                        Console.WriteLine($"Joe mama, here numba: {result} ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ur dum");
+                    }
+                }
+                Console.Write("Do you want to continue(y/n):");
+                value = Console.ReadLine();
+            }
+            while (value == "y" || value == "Y");
+        }
+
+        public static Operation? FindOperation(string symbol)
+        {
+            switch (symbol)
+            {
+                case "+":
+                     return Operation.Addition;
+
+                case "-":
+                    return Operation.Subtraction;
+
+                case "*":
+                    return Operation.Multiplication;
+
+                case "/":
+                    return Operation.Division;
+
+                default:
+                    Console.WriteLine("Wrong Operation");
+                    return null;
+            }
+        }
+    }
+}
